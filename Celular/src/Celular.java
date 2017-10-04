a39330fb61cc95c335e6ee0d2069c29a43fcb1f0
Juan <Juan@pc>
2017-10-04 17:17:29 -0500

    Codigo Celular

---
+++ Celular/src/Celular.java
@@ -0,0 +1,7 @@
+
+public class Celular {
+	boolean teclado;
+	String pantalla = "HD";
+	String carrier;
+
+}
---
+++ Celular/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Celular mov1 = new Celular();
+		System.out.println(mov1.pantalla);
+	}
+
+}
